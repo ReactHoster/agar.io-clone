42c222adcdfe1ed8785e1abd5e2cf979b318822e
ReactHoster <134359453+ReactHoster@users.noreply.github.com>
2023-07-07 10:36:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
     maxVirus: 50,
     slowBase: 4.5,
     logChat: 0,
-    networkUpdateFactor: 500,
+    networkUpdateFactor: 20,
     maxHeartbeatInterval: 5000,
     foodUniformDisposition: true,
     virusUniformDisposition: false,
